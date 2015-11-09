33084a635996f3f2766ce80d4465673993271dfd
Lei <tendant@gmail.com>
2015-11-08 19:48:54 -0800

    Initial Commit

---
+++ OpsMenu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OpsMenu
+//
+//  Created by MacMini on 11/8/15.
+//  Copyright © 2015 VariableName. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
